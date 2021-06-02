d8b4a25f75ad8b06871a817600a3da3670943b11
Carlos-DBarbosa <48024561+Carlos-DBarbosa@users.noreply.github.com>
2021-06-02 14:53:23 -0500

    Boton menu arreglado

--- RunGlass/RunGlass/Menu.gd
+++ RunGlass/RunGlass/Menu.gd
@@ -1,24 +1,16 @@
 extends Control
 
+export (PackedScene) var Scene
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
-
 func _on_play_pressed():
-	get_tree().change_scene("res://runglass.tscn")
-
+	get_tree().change_scene_to(Scene)
 
 
 func _on_salir_pressed():
